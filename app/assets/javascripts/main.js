a370ddde773963bf50c7aadb0434f2fbf814bd2a
marcdysart <dysart.marc@gmail.com>
2014-12-24 21:36:58 -0700

    Fixed ajax to add a new item to a list

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,37 +1,52 @@
 // Add a new Item
-//   attach an event to the form (listen for a submit event)
+
 $(document).ready(function(){
-      $( 'form' ).submit(function(event){
-          var data = $(this).serialize();
-          $.ajax({
-            type: "PUT",
-            url: $(this).attr('action'),
-            data: data,
-            dataType: 'json',
-            success: function(){
-              $('#item-table tbody').append(JSON.parse(response.body));
-              $('.new-item').reset();
-              });
-          })
-          event.preventDefault();
+      $( '#new_item' ).submit(handle_submit);
+      $( '#button-2').click(function() {alert("Button was clicked")
       });
+
 });
+
+
+
+function handle_submit(event){
+  var data = $(this).serialize();
+
+
+  $.ajax({
+    type: "POST",
+    url: $(this).attr('action'),
+    data: data,
+    dataType: 'json',
+    success: function(response){
+      console.log('got it'+response);
+      desc = response['item']['description'];
+      id =response['item'];
+      console.log(id);
+      item_num = 150;
+      console.log(item_num);
+      var new_row = $('<tr><td>'+desc+'</td><td><a data-method="delete" href= "/items/'+item_num+'" class="btn" id="button-2">Mark Complete</a></td></tr>');
+      $('#item-table tbody').append(new_row);
+      $('#item_description').val("");
+      new_button_listener;
+      // add_listener  - adds a listener to every button that is added to the page
+    }
+  });
+  event.preventDefault();
+}
+
+function new_button_listener(event){
+  $( '#button-2').click(function() {alert("Button was clicked")
+});
+}
+
+// Add mark complete button
 
 // Mark an Item completed.
 // $(document).ready(function(){
-//   $( 'form' ).submit(function(event){
-//     var data = $(this).serialize();
-//     $.ajax({
-//       type: "DELETE",
-//       url: $(this).attr('action'),
-//       data: data,
-//       dataType: 'json'
-//     }).success(function(){
-//       $('#item-table tbody').remove(JSON.parse(response.body));
-//     });
-//     event.preventDefault();
-//   });
+//   c
 // });
+// Need id from tr so it knows which one to remove
 
 // Create a new List.
 // $(document).ready(function(){
@@ -57,7 +72,7 @@ $(document).ready(function(){
 //   $( ".result" ).html( submit_new_item );
 // });
 
-
+//   attach an event to the form (listen for a submit event)
 //   assemble parameters
 //   make ajax call - POST
 //   on success do this:
